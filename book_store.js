a30e0ab461a8bc020db8fc2b7f24ae159090f38e
Ricardo <rr155914@gmail.com>
2021-07-05 15:37:13 -0500

    Add id button

--- book_store.js
+++ book_store.js
@@ -1,14 +1,27 @@
-const title = document.getElementById('#Title').value;
-const author = document.getElementById('#Author').value;
+// const title = document.getElementById('#Title').value;
+// const author = document.getElementById('#Author').value;
+// const bookList = [
+//     {
+//     title: 'Book 1',
+//     author: 'Alison',
+//     },
+//     {
+//     title: 'Book 1',
+//     author: 'Alison',
+//     }
+//     ]; 
 
-const bookList = [
-  {
-    title: 'Book 1',
-    author: 'Alison',
-  },
-  {
-    title: 'Book 1',
-    author: 'Alison',
-  }
-];
+   
+    
+     document.getElementById('#btn').addEventListener('submit', (e) => {
+            e.preventDefault();
+            const title = document.getElementById('#Title').value;
+            const author = document.getElementById('#Author').value;
+            const book = new book[title,author];
+            console.log(book);
+            });      
+    
 
+
+    
+  
