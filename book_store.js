c8c1c7405d9ba78d703d7762087fa70209cb30f6
Aduda-Boaz <adudaboaz@gmail.com>
2021-07-05 23:06:51 +0300

    Create a book list dammy data

--- book_store.js
+++ book_store.js
@@ -1,4 +1,14 @@
 const title = document.getElementById('#Title').value;
 const author = document.getElementById('#Author').value;
 
+const bookList = [
+  {
+    title: 'Book 1',
+    author: 'Alison',
+  },
+  {
+    title: 'Book 1',
+    author: 'Alison',
+  }
+];
 
---
+++ dist/book_store.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var title = document.getElementById('#Title').value;
+var author = document.getElementById('#Author').value;
+var bookList = [{
+  title: 'Book 1',
+  author: 'Alison'
+}, {
+  title: 'Book 1',
+  author: 'Alison'
+}];
