92b77abb9c0424ebe395c74dd213c1ab2731c3d0
Ricardo <rr155914@gmail.com>
2021-07-08 23:06:50 -0500

    refactor functions into constructors functions

--- book_store.js
+++ book_store.js
@@ -1,45 +1,61 @@
 /* eslint-disable no-unused-vars */
-const title = document.querySelector('.Book');
-const author = document.querySelector('.Book');
+/* eslint-disable max-classes-per-file */
+
+const titles = document.getElementById('Title');
+const authors = document.getElementById('Author');
 const listB = document.getElementById('book-list');
 const button = document.getElementById('btn');
+const libros = document.getElementById('Libreria');
+const remove = document.getElementById('btn-remove');
 let books = [];
-
-const popHtml = () => {
-  listB.innerHTML = '';
-  books = JSON.parse(localStorage.getItem('Libros'));
-
-  if (books === null) {
-    books = [];
-  } else {
-    for (let e = 0; e < books.length; e += 1) {
-      listB.innerHTML += `  <li class="book-title">${books[e].bookTitle}</li>
-      <li class="book-author">${books[e].author}</li>
-      <button id="btn-remove" onclick="remove(${e})">Remove</button>`;
-    }
+class Book {
+  constructor (title, author) {
+    this.title = title;
+    this.author = author;
   }
-};
-
-document.addEventListener('DOMContentLoaded', popHtml);
-
-function addBook(e) {
-  e.preventDefault();
-  const bookTitle = (this.querySelector('[name=book]')).value;
-  const author = (this.querySelector('[name=auth]')).value;
-  const book = {
-    bookTitle,
-    author,
-  };
-  this.reset();
-  books.push(book);
-  localStorage.setItem('Libros', JSON.stringify(books));
-  popHtml();
 }
 
-function remove(e) {
-  books.splice(e, 1);
-  localStorage.setItem('Libros', JSON.stringify(books));
-  popHtml();
+if (localStorage.getItem('Libros')!== null) {
+  JSON.parse(localStorage.getItem('Libros')).forEach((element) =>{
+    books.push(element);
+  });
+} else {
+  books = [];
+}
+class Library {
+  popHtml(books) {
+    this.books = books;
+    listB.innerHTML = '';
+    for (let e = 0; e < books.length; e += 1) {
+      listB.innerHTML += `  <li class="book-title">${books[e].title}</li>
+      <li class="book-author">${books[e].author}</li>
+      <button type="button" id="btn-remove" onclick="a.bookRemove(${e}, a)">Remove</button>`;
+  }
+  }
+
+  bookUpdate(x) {
+    this.x = x;
+    books.push(x);
+    localStorage.setItem('Libros', JSON.stringify(books));
+    return books;
+  }
+
+  bookRemove(e , j) {
+    this.e = e;
+    this.j = j;
+    books.splice (e, 1);
+    localStorage.setItem('Libros', JSON.stringify(books));
+    j.popHtml(books);
+  }
 }
 
-title.addEventListener('submit', addBook);
+const a = new Library();
+
+a.popHtml(books);
+button.addEventListener('click', () => {
+    const bookList = new Book(titles.value, authors.value);
+    a.bookUpdate(bookList);
+    a.popHtml(books);
+    document.getElementById('Libreria').reset();
+  });
+
